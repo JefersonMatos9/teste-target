29041264b22b0a8ac0a902c4718314b1c60f3015
JefersonMatos9 <decoo.matos@live.com>
2024-09-07 18:12:31 -0300

    .

--- src/main/java/exercicio05/Exercicio05.java
+++ src/main/java/exercicio05/Exercicio05.java
@@ -27,7 +27,7 @@ public class Exercicio05 {
         System.out.println("1. Ligue o Interruptor A e deixe-o ligado por alguns minutos.");
         System.out.println("2. Desligue o Interruptor A e ligue o Interruptor B.");
         System.out.println("3. Vá até a sala das lâmpadas.");
-        System.out.println("4. A lâmpada que está acesa e quente é controlada pelo Interruptor A.");
+        System.out.println("4. A lâmpada que está apagada e quente é controlada pelo Interruptor A.");
         System.out.println("5. A lâmpada que está acesa e fria é controlada pelo Interruptor B.");
         System.out.println("6. A lâmpada que está apagada e fria é controlada pelo Interruptor C.");
     }
