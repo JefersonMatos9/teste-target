dea07b9902c2d545f74a0ebe4881839c3c6b458a
JefersonMatos9 <decoo.matos@live.com>
2024-09-07 18:17:28 -0300

    .

--- src/main/java/exercicio05/Exercicio05.java
+++ src/main/java/exercicio05/Exercicio05.java
@@ -6,18 +6,6 @@ package exercicio05;
 
 public class Exercicio05 {
     public static void main(String[] args) {
-        // Explicação detalhada da solução
-        // Primeira Ida à Sala das Lâmpadas:
-        // 1. Ligue o Interruptor A e deixe-o ligado por alguns minutos para aquecer a lâmpada.
-        // 2. Após alguns minutos, desligue o Interruptor A e ligue o Interruptor B.
-        // 3. Deixe o Interruptor B ligado e vá até a sala das lâmpadas.
-
-        // Verificação na Sala das Lâmpadas:
-        // 1. A lâmpada que está acesa e quente é a lâmpada controlada pelo Interruptor A, pois foi aquecida enquanto o Interruptor A estava ligado.
-        // 2. A lâmpada que está acesa e fria é a lâmpada controlada pelo Interruptor B, pois foi ligada recentemente.
-        // 3. A lâmpada que está apagada e fria é a lâmpada controlada pelo Interruptor C, pois este interruptor não foi acionado.
-
-        // Exemplo de chamada de método para ilustrar a solução
 
         identificarInterruptores();
     }
@@ -28,7 +16,7 @@ public class Exercicio05 {
         System.out.println("2. Desligue o Interruptor A e ligue o Interruptor B.");
         System.out.println("3. Vá até a sala das lâmpadas.");
         System.out.println("4. A lâmpada que está apagada e quente é controlada pelo Interruptor A.");
-        System.out.println("5. A lâmpada que está acesa e fria é controlada pelo Interruptor B.");
+        System.out.println("5. A lâmpada que está acesa é controlada pelo Interruptor B.");
         System.out.println("6. A lâmpada que está apagada e fria é controlada pelo Interruptor C.");
     }
 }
